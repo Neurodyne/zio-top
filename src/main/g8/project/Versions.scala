e0e33fe609c199b34e4a0bba7a198386148e5e90
tampler <socnetfpga@gmail.com>
2020-07-06 12:15:18 +0300

    Bump vers

--- src/main/g8/project/Versions.scala
+++ src/main/g8/project/Versions.scala
@@ -1,5 +1,5 @@
 object Version {
-  val cats           = "2.2.0-M2"
-  val zio            = "1.0.0-RC21-1"
+  val cats           = "2.2.0-M3"
+  val zio            = "1.0.0-RC21-2"
   val zioInteropCats = "2.1.3.0-RC16"
 }
